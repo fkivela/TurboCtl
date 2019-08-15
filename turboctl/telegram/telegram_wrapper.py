8966975155fa48eb359f4842c8c9cf01021f0650
Feliks Kivelä <feliks.kivela@helsinki.fi>
2019-08-15 17:36:45 +0300

    Added docstrings to telegram_wrapper.py

--- turboctl/telegram/telegram_wrapper.py
+++ turboctl/telegram/telegram_wrapper.py
@@ -1,11 +1,26 @@
+"""This module provides a more user friendly intefrace to the Telegram 
+class.
+"""
 from ..data import (Types, PARAMETERS, ParameterAccess, ParameterResponse, 
                     ParameterError, ControlBits, StatusBits)    
 
 from .telegram import Telegram
-#from .wrapper import NotifyUponAccess
 
 
 class TelegramWrapper(Telegram):
+    """An abstract superclass for Query and Reply objects.
+    
+    Query and Reply objects provide a layer of abstraction on top of 
+    Telegram objects. Parameters can be accessed by setting the 
+    *parameter_number* and *parameter_mode* properties of a 
+    TelegramWrapper class, without knowing the size or type of the 
+    parameter. Control and status bits can be defined as a set of 
+    enum instances instead of a string of '1's and '0's.
+    
+    telegram_wrapper.control_or_status_set is a SynchronizedSet object 
+    which behaves lika an ordinary set, but any changes to it are 
+    instantly copied to telegram_wrapper.control_or_status_bits.
+    """
         
     READABLE_PROPERTIES = Telegram.READABLE_PROPERTIES + [
         'parameter_type', 
@@ -20,18 +35,27 @@ class TelegramWrapper(Telegram):
         'control_or_status_set']
     
     parameters = PARAMETERS
+    # The set of parameters can be changed for testing purposes.
     enum = None
+    # *enum* is set in subclasses.
     
     def __init__(self, *args, **kwargs):
+        """Initialize a new TelegramWrapper.
+        All arguments are passed to the Telegram class.
+        """
         self._control_or_status_set = SynchronizedSet(self._update_cs_bits)
         self.cs_bit_lock = False
         
+        # *parameter_number* can't be changed after initialization, 
+        # because the values of other properties depend on it.
         self._parameter_number_is_set = False
         super().__init__(*args, **kwargs)
         self._parameter_number_is_set = True
         
     def _set_kwargs(self, **kwargs):
+        """Initialize properties given as keyword arguments."""
         try:
+            # Set *parameter_number* before other kwargs.
             self.parameter_number = kwargs['parameter_number']
             kwargs.pop('parameter_number')
         except KeyError:
@@ -40,6 +64,9 @@ class TelegramWrapper(Telegram):
     
     @Telegram.parameter_number.setter        
     def parameter_number(self, value):
+        """Set self.parameter_number, or raise a RuntimeError if this 
+        method is called after initialization.
+        """
         if self._parameter_number_is_set:
             raise RuntimeError(
                 'parameter_number cannot be set after initialization')
@@ -47,6 +74,9 @@ class TelegramWrapper(Telegram):
     
     @property
     def _parameter(self):
+        """Return a parameter object or None, if a parameter 
+        corresponding to self.parameter_number doesn't exist.
+        """
         try:
             return self.parameters[self.parameter_number]
         except KeyError:
@@ -54,30 +84,53 @@ class TelegramWrapper(Telegram):
         
     @property
     def parameter_type(self):
+        """Return the type of the parameter (a Types enum instance), 
+        or Types.UINT if a parameter corresponding to 
+        self.parameter_number doesn't exist.
+        """
         if not self._parameter:
             return Types.UINT
         return self._parameter.type
     
     @property
     def parameter_size(self):
+        """Return the size of the parameter in bits, or 0 if a 
+        parameter corresponding to self.parameter_number 
+        doesn't exist.
+        """
         if not self._parameter:
             return 0
         return self._parameter.size
     
     @property
     def parameter_unit(self):
+        """Return the unit of the parameter (a string), or '' if a
+        parameter corresponding to self.parameter_number doesn't exist.
+        """
         if not self._parameter:
             return ''
         return self._parameter.unit
     
     @property
     def parameter_indexed(self):
+        """Return a boolean describing whether the parameter is 
+        indexed (return True) or not (return False), or False if a
+        parameter corresponding to self.parameter_number doesn't exist.
+        """
         if not self._parameter:
             return False
         return bool(self._parameter.indices)
         
     @property
     def parameter_value(self):
+        """Get or set the value of the parameter.
+        
+        The getter returns a positive or negative int or a float; the 
+        return type is set automatically according to the parameter.
+        
+        The setter raises a TypeError if the type of *value* doesn't 
+        match the parameter.
+        """
         return self.get_parameter_value(self.parameter_type)
     
     @parameter_value.setter
@@ -94,6 +147,9 @@ class TelegramWrapper(Telegram):
         
     @property        
     def parameter_mode(self):
+        """Get or set parameter access or response mode. 
+        This property is defined the subclasses of this class.
+        """
         raise NotImplementedError('This is an abstract function left '
                                   'to be defined in subclasses.')
         
@@ -104,6 +160,15 @@ class TelegramWrapper(Telegram):
     
     @property
     def control_or_status_set(self):
+        """Get or set control or status bits as a set of enum 
+        instances.
+        
+        The set returned by the getter is a SynchronizedSet object, 
+        which updates self.control_or_status_set every time it is changed.
+        
+        Setting this property to a new set updates the SynchronizedSet 
+        object instead of replacing it.
+        """
         return self._control_or_status_set
     
     @control_or_status_set.setter
@@ -113,6 +178,9 @@ class TelegramWrapper(Telegram):
     
     @property
     def control_or_status_bits(self):
+        """Get or set self.control_or_status_bits, and update 
+        self.control_or_status_set accordingly.
+        """
         return super().control_or_status_bits
     
     @control_or_status_bits.setter
@@ -121,31 +189,51 @@ class TelegramWrapper(Telegram):
         self._update_cs_set()
     
     def _update_cs_bits(self):
+        """Update self.control_or_status_bits to match 
+        self.control_or_status_set.
+        """
         if self.cs_bit_lock:
             return
         
         indices = [i.value for i in self.control_or_status_set]        
         bits = ''.join(['1' if i in indices else '0' for i in range(16)])
         
+        # The lock prevents a recursive loop where accessing 
+        # self.control_or_status_set calls this method again.
         self.cs_bit_lock = True
         self.control_or_status_bits = bits    
         self.cs_bit_lock = False
         
     def _update_cs_set(self):
+        """Update self.control_or_status_set to match 
+        self.control_or_status_bits.
+        """
         if self.cs_bit_lock:
             return
 
         bits = self.control_or_status_bits
         set_ = {self.enum(i) for i, bit in enumerate(bits) if bit == '1'}
         
+        # The lock prevents a recursive loop where accessing 
+        # self.control_or_status_bits calls this method again.
         self.cs_bit_lock = True
         self.control_or_status_set.update(set_)
         self.cs_bit_lock = False    
         
         
 class SynchronizedSet(set):
+    """Objects of this class behave like a set, but call 
+    self.upon_update every time the contents of the set are changed.
+    """
     
     def __init__(self, upon_update, *args, **kwargs):
+        """Initialize a new SynchronizedSet.
+        
+        Args:
+            upon_update: A function to be run whenever a method that 
+                can change the contents of this set is called.
+            Other arguments are passed to the set class.
+        """
         super().__init__(*args, **kwargs)
         self.upon_update = upon_update
 
@@ -156,10 +244,13 @@ class SynchronizedSet(set):
         ]
         for name in update_methods:
             method = getattr(self, name)
-            new_method = self.add_upon_update(method)
+            new_method = self._add_upon_update(method)
             setattr(self, name, new_method)
         
-    def add_upon_update(self, method):
+    def _add_upon_update(self, method):
+        """Add a call to self.upon_update to the end of *method* and 
+        return the result.
+        """
         def new_method(*args, **kwargs):
             method(*args, **kwargs)
             self.upon_update()
@@ -167,11 +258,24 @@ class SynchronizedSet(set):
 
                                     
 class Query(TelegramWrapper):
+    """This class provides a user-friendly interface to telegrams sent 
+    to the pump.
+    """
     
     enum = ControlBits
     
     @property
     def parameter_mode(self):
+        """Get or set parameter access mode. 
+        
+        Setting this parameter automatically sets
+        self.parameter_access_type to the correct access code,
+        and the getter returns a word corresponding to that code.
+        
+        Valid values are 'none', 'read', 'write' and 'invalid'. 
+        A value of 'invalid' signifies an unrecognized parameter 
+        access code.
+        """
         code = self.parameter_access_type
         try:
             mode_ = ParameterAccess(code)
@@ -249,6 +353,9 @@ class Reply(TelegramWrapper):
     
     @property
     def error_message(self):
+        """Return an error message corresponding to the parameter 
+        error code, or '' if self.parameter_mode is not 'error'.
+        """
         if self.parameter_mode != 'error':
             return ''
         
@@ -259,6 +366,15 @@ class Reply(TelegramWrapper):
         
     @property
     def error_code(self):
+        """Set or get the parameter error code.
+        
+        The parameter error code is written to the same location as 
+        the parameter value. This means that the error code will be 
+        nonsensical if the parameter value is defined and vice versa.
+        
+        Setter raises:
+            TypeError: If *value* is not a valid unsigned integer.
+        """
         return self.get_parameter_value(Types.UINT)
         
     @error_code.setter
@@ -273,6 +389,17 @@ class Reply(TelegramWrapper):
     
     @property
     def parameter_mode(self):
+        """Get or set parameter response mode. 
+        
+        Setting this parameter automatically sets
+        self.parameter_access_type to the correct response code,
+        and the getter returns a word corresponding to that code.
+
+        Valid values are 'none', 'response', 'error', 'no write' and 
+        'invalid'. A value of 'invalid' signifies an unrecognized 
+        parameter response code.
+        """
+
         code = self.parameter_access_type
         try:
             mode_ = ParameterResponse(code)
