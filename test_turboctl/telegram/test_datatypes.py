91ceda054128b9a8a57bb92010f0f13f7ab973ed
Feliks Kivelä <feliks.kivela@helsinki.fi>
2020-06-05 15:04:40 +0300

    Added new tests to test_datatypes

--- test_turboctl/telegram/test_datatypes.py
+++ test_turboctl/telegram/test_datatypes.py
@@ -1,12 +1,20 @@
-"""Unit tests for the datatypes module."""
+"""Unit tests for the datatypes module.
+
+TODO: the following tests should be added:
+    - make sure init with an invalid value produce a ValueError
+    - make sure init with an invalid bits does the same
+    - make sure init works correctly if bits is not specified
+"""
 
 import math
 import unittest
+
 from hypothesis import given, assume, strategies as st
 
-from turboctl.telegram.datatypes import maxuint, maxsint, minsint, Data, Uint, Sint, Float, Bin
+from turboctl.telegram.datatypes import (
+    maxuint, maxsint, minsint, Uint, Sint, Float, Bin)
 
-# Testing with large numbers has no benefit and slows the tests
+# Testing with large numbers provides no benefits and slows the tests
 # down considerably.
 MAX_BITS = 64
 MAX_UINT = maxuint(MAX_BITS)
@@ -38,6 +46,15 @@ def values_and_bits(draw, class_, min_bits=0, max_bits=MAX_BITS):
         value = draw(st.from_regex(f'\A[01]{{{bits}}}\Z'))
         
     return value, bits
+
+@st.composite
+def iterables_and_bits(draw, min_bits=0, max_bits=MAX_BITS):
+    """Generate tuples of valid arguments for initializing Bin
+    objects from iterables.
+    """
+    bits = draw(st.integers(min_value=min_bits, max_value=max_bits))
+    iterable = draw(st.iterables(st.integers(min_value=0, max_value=bits)))
+    return iterable, bits
 
 
 @st.composite
@@ -151,7 +168,7 @@ class TestMagicMethods(unittest.TestCase):
         self.assertEqual(len(bytes_), obj.n_bytes)
         
     @given(data_objects(),
-           data_types(classes=(Uint, Sint, Bin)))
+            data_types(classes=(Uint, Sint, Bin)))
     def test_nested_bytes_when_class_is_not_float(self, obj, class_):
         bytes1 = bytes(obj)
         bytes2 = bytes(class_(bytes(obj)))
@@ -195,8 +212,8 @@ class TestMagicMethods(unittest.TestCase):
     # __eq__ should work also against non-Data objects,
     # which is why this is tested with a few integers.
     @given(data_objects(), 
-           st.one_of(data_objects(), 
-                     st.integers(min_value=-10, max_value=10)))
+            st.one_of(data_objects(), 
+                      st.integers(min_value=-10, max_value=10)))
     def test_different_type_value_or_bits_breaks_equality(self, obj1, obj2):
         try:
             both_values_nan = math.isnan(obj1.value) and math.isnan(obj2.value)
@@ -243,275 +260,52 @@ class TestInit(unittest.TestCase):
         self.assertEqual(obj.bits, other.bits)
     
     @given(values_and_bits(Uint))
-    def test_init_from_int_sets_attributes_correctly(self, i_and_bits):
+    def test_uint_from_int_sets_attributes_correctly(self, i_and_bits):
         i, bits = i_and_bits
         uint = Uint(i, bits)
         self.assertEqual(uint.value, i)
         self.assertEqual(uint.bits, bits)
         
-
+    @given(values_and_bits(Sint))
+    def test_sint_from_int_sets_attributes_correctly(self, i_and_bits):
+        i, bits = i_and_bits
+        sint = Sint(i, bits)
+        self.assertEqual(sint.value, i)
+        self.assertEqual(sint.bits, bits)
+        
+    @given(values_and_bits(Float))
+    def test_float_from_float_sets_attributes_correctly(self, x_and_bits):
+        x, bits = x_and_bits
+        float_ = Float(x, bits)
+        
+        if math.isnan(x):
+            self.assertTrue(math.isnan(float_.value))
+        else:
+            self.assertEqual(float_.value, x)
+        
+        self.assertEqual(float_.bits, bits)
+        
+    @given(values_and_bits(Bin))
+    def test_bin_from_str_sets_attributes_correctly(self, str_and_bits):
+        s, bits = str_and_bits
+        bin_ = Bin(s, bits)
+        
+        self.assertEqual(bin_.value, s)        
+        self.assertEqual(bin_.bits, bits)
+        
+    @given(iterables_and_bits())
+    def test_bin_from_iterable_sets_attributes_correctly(self, it_and_bits):
+        it, bits = it_and_bits
+        # Some iterables (i.e. generators) can only be iterated once, 
+        # so we need to save the contents to a list.
+        list_ = list(it)
+        bin_ = Bin(list_, bits)
+        
+        for i, c in enumerate(bin_.value):
+            self.assertEqual(c, '1' if i in list_ else '0')
+  
+        self.assertEqual(bin_.bits, bits)
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-
-# class TestUint(GenericTests):
-    
-#     def test_init(self):
-#         args = {(      1,   ):       1,
-#                 (    255,   ):     255,
-#                 (2**16-1, 16): 2**16-1,
-#                 (    '5',   ):       5,
-#                 (    1.2,   ):       1}        
-#         self._test_args(Uint, args)
-        
-#     def test_type(self):
-#         self.assertIsInstance(Uint(1), Uint)
-#         self.assertIsInstance(Uint.from_bytes(bytes([1])), Uint)
-    
-#     def test_inheritance(self):
-#         x = Uint(5)
-#         self.assertEqual(x, 5)
-#         self.assertEqual(x + 1, 6)
-#         self.assertEqual(x**2, 25)
-
-#     def test_bits(self):
-#         self.assertEqual(Uint(5).bits, 8)
-#         self.assertEqual(Uint(100, 10).bits, 10)
-        
-#     def test_invalid_args(self):
-#         args = {(   -1,   ): ValueError,
-#                 (  256,   ): ValueError,
-#                 (2**16, 16): ValueError}
-#         self._test_args(Uint, args)
-
-#     def test_to_bytes(self):
-#         # 500 = 00000001 11110100
-#         # Uints with sizes that are not multiples of 8 are padded 
-#         # with zeroes.
-#         args = {(  5,   ): bytes([5]),
-#                 (500, 16): bytes([1, 244]),
-#                 (500,  9): bytes([1, 244])}        
-#         def to_bytes(*args):
-#             return Uint(*args).to_bytes()        
-#         self._test_args(to_bytes, args)
-
-#     def test_from_bytes(self):
-#         args = {(bytes([5]     ),): Uint(  5,  8),
-#                 (bytes([1, 244]),): Uint(500, 16)}        
-#         self._test_args(Uint.from_bytes, args)
-        
-#     def test_to_builtin(self):
-#         i = Uint(5).to_builtin()
-#         self.assertEqual(i, 5)
-#         self.assertIsInstance(i, int)
-#         self.assertNotIsInstance(i, Uint)
-
-
-
-# class TestTurboNum(GenericTests):
-    
-#     def test_init(self):
-#         args = {(123      ,): Uint(123),
-#                 (-123     ,): Sint(-123),
-#                 (123.456  ,): Float(123.456),
-#                 ('1111011',): Bin('1111011')}
-#         def to_type(*args):
-#             return TurboNum(*args)     
-#         self._test_args(to_type, args)
-        
-#     def test_invalid_args(self):
-#         args = {(256   ,  ): ValueError,
-#                 (-129  ,  ): ValueError,
-#                 (1E40  ,  ): ValueError,
-#                 ('1111', 3): ValueError}
-#         def to_type(*args):
-#             return TurboNum(*args)     
-#         self._test_args(to_type, args)
-        
-#     def test_to(self):
-#         self.assertEqual(Uint(123).to(Bin), Bin('1111011', 8))
-#         self.assertEqual(Float(123.456).to(Uint), Uint(1123477881, 32))
-
-        
-
-# class TestSint(GenericTests):
-    
-#     def test_init(self):
-#         args = {(  -128,   ):   -128,
-#                 (   127,   ):    127,
-#                 (-2**15, 16): -2**15,
-#                 (  '-5',   ):     -5,
-#                 (  -1.2,   ):     -1}        
-#         self._test_args(Sint, args)
-        
-#     def test_type(self):
-#         self.assertIsInstance(Sint(-1), Sint)
-#         self.assertIsInstance(Sint.from_bytes(bytes([255])), Sint)
-        
-#     def test_inheritance(self):
-#         x = Sint(-5)
-#         self.assertEqual(x, -5)
-#         self.assertEqual(x + 1, -4)
-#         self.assertEqual(x**2, 25)
-                
-#     def test_bits(self):
-#         self.assertEqual(Sint(-5).bits, 8)
-#         self.assertEqual(Sint(-100, 10).bits, 10)
-        
-#     def test_invalid_args(self):
-#         args = {( -129,   ): ValueError,
-#                 (  128,   ): ValueError,
-#                 (2**15, 16): ValueError}
-#         self._test_args(Sint, args)
-        
-#     def test_to_bytes(self):
-#         # -5 = 11111011
-#         # -500 = 11111110 00001100
-#         args = {(  -5,   ): bytes([251]),
-#                 (-500, 16): bytes([254, 12]),
-#                 (-500, 10): bytes([254, 12])}        
-#         def to_bytes(*args):
-#             return Sint(*args).to_bytes()        
-#         self._test_args(to_bytes, args)
-        
-#     def test_from_bytes(self):
-#         args = {(bytes([251]    ),): Sint(  -5,  8),
-#                 (bytes([254, 12]),): Sint(-500, 16),
-#                 (bytes([2, 12]  ),): Sint( 524, 16)}        
-#         self._test_args(Sint.from_bytes, args)
-        
-#     def test_to_builtin(self):
-#         i = Sint(-5).to_builtin()
-#         self.assertEqual(i, -5)
-#         self.assertIsInstance(i, int)
-#         self.assertNotIsInstance(i, Sint)
-        
-        
-# class TestFloat(GenericTests):
-    
-#     def test_init(self):
-#         args = {( 123.456,   ): 123.456,
-#                 (-123.456, 32): -123.456,
-#                 (   1.2E3,   ): 1.2E3,
-#                 ('123.456',  ): 123.456,
-#                 ('1E5',    32): 1E5,
-#                 ('inf',      ): float('inf')}        
-#         self._test_args(Float, args)
-        
-#     def test_type(self):
-#         self.assertIsInstance(Float(0.0), Float)
-#         self.assertIsInstance(Float.from_bytes(bytes([0,0,0,0])), Float)
-        
-#     def test_inheritance(self):
-#         x = Float(123.456)
-#         self.assertAlmostEqual(x,     123.456,      delta=1E-5)
-#         self.assertAlmostEqual(x + 1, 124.456,      delta=1E-5)
-#         self.assertAlmostEqual(x**2,  15241.383936, delta=1E-2)
-        
-#     def test_bits(self):
-#         self.assertEqual(Float(1.0).bits, 32)
-#         self.assertEqual(Float(1E30).bits, 32)
-        
-#     def test_invalid_args(self):
-#         args = {( 1E50,   ): ValueError,
-#                 (-1E50,   ): ValueError,
-#                 (  0.0, 16): ValueError}
-#         self._test_args(Float, args)
-        
-#     def test_to_bytes(self):
-#         # 123.456 = 01000010 11110110 11101001 01111001
-#         # = [66, 246, 233, 121]
-#         args = {(123.456,): bytes([66, 246, 233, 121])}        
-#         def to_bytes(*args):
-#             return Float(*args).to_bytes()        
-#         self._test_args(to_bytes, args)
-        
-#     def test_from_bytes(self):
-#         args = {(bytes([66, 246, 233, 121]),): Float(123.456)}        
-#         self._test_args(Float.from_bytes, args)
-        
-#     def test_to_builtin(self):
-#         x = Float(123.456).to_builtin()
-#         self.assertAlmostEqual(x, 123.456, 5)
-#         self.assertIsInstance(x, float)
-#         self.assertNotIsInstance(x, Float)
-        
-        
-# class TestBin(GenericTests):
-    
-#     def test_init(self):
-#         args = {(    '10111101',   ):         '10111101',
-#                 ('111100001111', 16): '0000111100001111',
-#                 (          101 ,   ):              '101',
-#                 (          101 ,  5):            '00101'}        
-#         self._test_args(Bin, args)
-        
-#     def test_type(self):
-#         self.assertIsInstance(Bin('1'), Bin)
-#         self.assertIsInstance(Bin.from_bytes(bytes([1])), Bin)
-        
-#     def test_inheritance(self):
-#         x = Bin('10111101')
-#         self.assertEqual(x[0:2], '10')
-#         self.assertEqual(x + 'X', '10111101X')
-#         self.assertTrue('1' in x)
-        
-#     def test_addition(self):
-#         s1 = Bin('000')
-#         s2 = Bin('111')
-#         self.assertEqual(s1 + s2, '000111')
-#         self.assertIsInstance(s1 + s2, Bin)
-#         s1 += s2
-#         self.assertEqual(s1, '000111')
-#         self.assertIsInstance(s1, Bin)
-        
-#     def test_indexing(self):
-#         s = Bin('1001')
-#         self.assertEqual(s[0], '1')
-#         self.assertIsInstance(s[0], Bin)
-#         self.assertEqual(s[1:3], '00')
-#         self.assertIsInstance(s[1:3], Bin)
-        
-#     def test_bits(self):
-#         self.assertEqual(Bin(8*'1').bits, 8)
-#         self.assertEqual(Bin(16*'1').bits, 16)
-#         self.assertEqual(Bin('1', 5).bits, 5)
-        
-#     def test_invalid_args(self):
-#         args = {('102',  ): ValueError,
-#                 (9*'1', 8): ValueError}
-#         self._test_args(Bin, args)
-        
-#     def test_to_bytes(self):
-#         # 123.456 = 01000010 11110110 11101001 01111001
-#         # = [66, 246, 233, 121]
-#         args = {('10111101' + '00111100', 16): bytes([189, 60]),
-#                 ('100111100'           ,   9): bytes([1, 60])}        
-#         def to_bytes(*args):
-#             return Bin(*args).to_bytes()        
-#         self._test_args(to_bytes, args)
-        
-#     def test_from_bytes(self):
-#         args = {(bytes([189, 60]),): Bin('10111101' + '00111100', 16),
-#                 (bytes([1, 60]  ),): Bin('00000001' + '00111100', 16)}        
-#         self._test_args(Bin.from_bytes, args)
-        
-#     def test_to_builtin(self):
-#         s = Bin('10111101').to_builtin()
-#         self.assertAlmostEqual(s, '10111101')
-#         self.assertIsInstance(s, str)
-#         self.assertNotIsInstance(s, Bin)
-
-
-
 if __name__ == '__main__':
     unittest.main()
